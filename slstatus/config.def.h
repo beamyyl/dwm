e2b84e43cda3e8a43094eac324fdd102c44f69ad
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-03-20 15:39:49 +0200

    Update config.def.h

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -65,11 +65,7 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function      format       argument */
 	{ run_command,   "%s",        "wpctl get-volume @DEFAULT_SINK@ | awk '{if ($NF==\"[MUTED]\") print \"󰝟 muted\"; else { v=$2*100; if (v<30) printf \"󰕿 %d%%\", v; else if (v<70) printf \"󰖀 %d%%\", v; else printf \"󰕾 %d%%\", v}}'" },
-	{ run_command,   " | %s",    "echo ''" },
-	{ run_command,   "%s",       "p=$(cat /sys/class/power_supply/BAT0/capacity); s=$(cat /sys/class/power_supply/BAT0/status); if [ \"$s\" = \"Charging\" ]; then printf \"󰂈 %d%%\" $p; elif [ \"$s\" = \"Full\" ]; then printf \"󰁹 %d%%\" $p; elif [ $p -le 10 ]; then printf \"󰂎 %d%%\" $p; elif [ $p -le 25 ]; then printf \"󰁺 %d%%\" $p; elif [ $p -le 50 ]; then printf \"󰁼 %d%%\" $p; elif [ $p -le 75 ]; then printf \"󰁾 %d%%\" $p; else printf \"󰂀 %d%%\" $p; fi" },
-	{ run_command,   " | %s",    "echo ''" },
-	{ datetime,      "󰥔 %s  ",   "%H:%M  󰃭 %Y-%m-%d" },
+	{ run_command,   " | %s",     "cat /sys/class/power_supply/BAT0/capacity | tr -d '\\n' | xargs -I{} sh -c 's=$(cat /sys/class/power_supply/BAT0/status); p={}; if [ \"$s\" = \"Charging\" ]; then printf \"󰂈 %d%%\" $p; elif [ \"$s\" = \"Full\" ]; then printf \"󰁹 %d%%\" $p; elif [ $p -le 10 ]; then printf \"󰂎 %d%%\" $p; elif [ $p -le 25 ]; then printf \"󰁺 %d%%\" $p; elif [ $p -le 50 ]; then printf \"󰁼 %d%%\" $p; elif [ $p -le 75 ]; then printf \"󰁾 %d%%\" $p; else printf \"󰂀 %d%%\" $p; fi'" },
+	{ datetime,      " | 󰥔 %s  ", "%H:%M  󰃭 %Y-%m-%d" },
 };
-
